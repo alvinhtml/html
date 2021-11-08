77d1fe8dc2826e83e7b3117af22baf36d3b5f897
Alvin <alvinhtml@gmail.com>
2021-11-08 22:38:28 +0800

    add style for example page

--- src/websocket/agent.js
+++ src/websocket/agent.js
@@ -1,11 +1,18 @@
 const WebSocket = require('ws');
 
-const ws = new WebSocket('ws://localhost:7060');
+// const ws = new WebSocket('wss://192.168.14.70:9001');
+
+
+const ws = new WebSocket('wss://192.168.14.70:9001', {
+  protocolVersion: 8,
+  origin: 'https://192.168.14.70:9001',
+  rejectUnauthorized: false
+})
 
 ws.on('open', function open() {
-  ws.send('Hi Server');
+  ws.send('Hi Server')
 });
 
 ws.on('message', function incoming(data) {
-  console.log(data);
+  console.log(data)
 });
--- src/websocket/server.js
+++ src/websocket/server.js
@@ -1,15 +1,62 @@
-const WebSocket = require('ws');
+// wss.js
 
-const wss = new WebSocket.Server({ port: 7060 });
+const fs = require('fs');
 
-wss.on('connection', function connection(ws) {
+// 一些配置信息
+const cfg = {
+    port: 8888,
+    ssl_key: 'ssl.key',
+    ssl_cert: 'ssl.crt'
+};
 
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    ws.send('Hi Client');
-  });
+const httpServ = require('https');
+const WebSocketServer = require('ws').Server; // 引用Server类
 
-  setInterval(() => {
-    ws.send('Hi Client' + new Date());
-  }, 3000)
+// 创建request请求监听器
+const processRequest = (req, res) => {
+    res.writeHead(200);
+    res.end('厉害了，我的WebSockets!\n');
+};
+
+const app = httpServ.createServer({
+    // 向server传递key和cert参数
+    key: fs.readFileSync(cfg.ssl_key),
+    cert: fs.readFileSync(cfg.ssl_cert)
+}, processRequest).listen(cfg.port);
+
+// 实例化WebSocket服务器
+const wss = new WebSocketServer({
+    server: app
 });
+// 如果有WebSocket请求接入，wss对象可以响应connection事件来处理
+wss.on('connection', (wsConnect) => {
+    console.log('服务器已启动，监听中~');
+    wsConnect.on('message', (message) => {
+        console.log(`服务器接收到：${message}`);
+        wsConnect.send(`服务器回复: ${message}`, (err) => {
+            if (err) {
+                console.log(`服务器错误：${err}`);
+            }
+        });
+    });
+});
+
+
+
+
+
+// const WebSocket = require('ws');
+//
+// const wss = new WebSocket.Server({ port: 7060 });
+//
+// wss.on('connection', function connection(ws) {
+//
+//   ws.on('message', function incoming(message) {
+//     console.log('received: %s', message);
+//     ws.send('Hi Client');
+//   });
+//
+//   setInterval(() => {
+//     ws.send('Hi Client' + new Date());
+//   }, 3000)
+// });
